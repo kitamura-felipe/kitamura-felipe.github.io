1aa4e8956b5316ee26af411f17c41fcd51461607
Eduardo Farina <eduardofarina61@gmail.com>
2025-08-28 08:24:34 -0300

    updating url to https://f7b12657bf9b.ngrok-free.app

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const url = "https://cb95-2804-7f0-ba01-27b7-49e4-e00a-5bbb-598d.ngrok-free.app";
+    const url = "https://f7b12657bf9b.ngrok-free.app";
     const apps = [
         { name: "Bone Age Report Generator (Portuguese)", image: "img/app-1.jpg", link: url },
         { name: "Chest X-ray Report Generator (demo)", image: "img/app-4.jpg", link: "https://huggingface.co/spaces/eduardofarina/MultimodalCXray" },
